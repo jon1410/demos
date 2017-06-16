3a08fc44c87a5ad8e711173684df5cd08ff1176d
Ivan <ivanjovic@msn.com>
2017-06-16 11:44:27 +0200

    MVC Config removed, use Spring Boot default MVC Config

--- src/main/java/org/ijovic/demo/orientdb/app/config/SpringBootOrientdbDemoApplication.java
+++ src/main/java/org/ijovic/demo/orientdb/app/config/SpringBootOrientdbDemoApplication.java
@@ -19,18 +19,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableTransactionManagement
 @ComponentScan("org.ijovic.demo")
 @EnableOrientRepositories("org.ijovic.demo.orientdb.repository")
-@EnableWebMvc
-public class SpringBootOrientdbDemoApplication extends WebMvcConfigurerAdapter{
+public class SpringBootOrientdbDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootOrientdbDemoApplication.class, args);
-	}
-
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry
-				.addResourceHandler("/webjars/**")
-				.addResourceLocations("/webjars/", "classpath:/META-INF/resources/webjars/");
 	}
 
 }
